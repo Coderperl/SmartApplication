4f0dc35d3e2b00262362cb9494604719357a5603
Per görtz <pelle_gortz@hotmail.com>
2022-09-23 16:42:38 +0200

    Add project files.

---
+++ SmartApplication/MVVM/Models/DeviceItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApplication.MVVM.Models
+{
+    internal class DeviceItem
+    {
+        public string DeviceId { get; set; } = "";
+        public string DeviceName { get; set; } = "";
+        public string DeviceType { get; set; } = "";
+
+        public string ActiveIcon { get; set; } = "";
+        public string InactiveIcon { get; set; } = "";
+        public string StateActive { get; set; } = "";
+        public string StateInactive { get; set; } = "";
+    }
+}
---
+++ SmartApplication/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApplication.MVVM.ViewModels
+{
+    internal class MainViewModel :ObservableObject
+    {
+        private object _currentView;
+        public RelayCommand KitchenViewCommand { get; set; }
+        public RelayCommand BedRoomViewCommand { get; set; }
+        public RelayCommand LivingRoomRelayCommand { get; set; }
+        public KitchenViewModel KitchenViewModel { get; set; }
+        public BedRoomViewModel BedRoomViewModel { get; set; }
+        public LivingRoomViewModel LivingRoomViewModel { get; set; }
+
+
+        public MainViewModel()
+        {
+            KitchenViewModel = new KitchenViewModel();
+            BedRoomViewModel = new BedRoomViewModel();
+            LivingRoomViewModel = new LivingRoomViewModel();
+
+            KitchenViewCommand = new RelayCommand(x => { CurrentView = KitchenViewModel; });
+            BedRoomViewCommand = new RelayCommand(x => { CurrentView = BedRoomViewModel; });
+            LivingRoomRelayCommand = new RelayCommand(x => { CurrentView = LivingRoomViewModel; });
+        }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+    }
+}
